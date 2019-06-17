d7149dff38e99d3b625fe69d29aef6f7f9daf0a7
Leonardo Aravena Cuevas <ld.aravena@gmail.com>
2019-06-17 15:27:36 -0400

    Update Map.h

--- Map.h
+++ Map.h
@@ -10,10 +10,10 @@ class Map{
     public:
 
         virtual void insert(std::pair<std::string,int>) = 0;
-		virtual int at(std::string) = 0;
+	virtual int at(std::string) = 0;
         virtual void erase(std::string) = 0;
-		virtual int size() = 0;
-		virtual bool empty() = 0;
+	virtual int size() = 0;
+	virtual bool empty() = 0;
 };
 
 #endif
